8b426060249b7498c1f680659d1703d126fdb956
Nikita <nekitrus92@yandex.ru>
2022-10-07 16:51:09 +0300

    PRA-37: gameScreen

--- packages/client/src/pages/game/game.tsx
+++ packages/client/src/pages/game/game.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
+import { start } from '../hook';
 import './game.scss'
+import foto from '../../assets/222.jpg'
 const Game: React.FC = () => {
     const [IsGameStarted, setIsGameStarted] = useState(false)
 
@@ -14,46 +16,62 @@ const Game: React.FC = () => {
             const contextFigure = canvasFigure.getContext('2d')
             if (context && contextFigure) {
                 context.fillStyle = '#B0E0E6'
+                // context.textAlign = "center";
+                // context.textBaseline = "middle";
+                // context.strokeText("Game Over", 30, 50);
                 context.fillRect(0, 0, context.canvas.width, context.canvas.height)
-                contextFigure.fillStyle = '#B0E0E6'
-                contextFigure.fillRect(0, 0, context.canvas.width, context.canvas.height)
+                // contextGameOver.strokeText('Game Over', 10, 100);
+                // contextGameOver.font = "40px Courier";
+                // contextGameOver.fillStyle = '#000'
+
+                // contextFigure.fillRect(0, 0, context.canvas.width, context.canvas.height)
             }
         }
     }, [])
+    const startGame = () => {
+        setIsGameStarted(true)
+        start()
+    }
+
     return (
         <div className='game-container'>
             <div className='menu-game'>
                 <h2>меню</h2>
-                <p>пункт меню</p>
-                <p>пункт меню</p>
-                <p>пункт меню</p>
-                <p>пункт меню</p>
-                <p>пункт меню</p>
+                <li>новая игра</li>
+                <li>как играть</li>
+                <li>доска почета</li>
+                <li>мой профиль</li>
+                <li>форум</li>
+                <li>ночная тема</li>
+                <li>настройки</li>
+                <li>выйти</li>
             </div>
             <div className='game-screen'>
                 <canvas ref={canvasRef} id="canvas" ></canvas>
-                {!IsGameStarted &&
-                    <div className="button-wrapper">
-                        <div className="button-wrapper-inner">
-                            <button className='start-btn' onClick={() => setIsGameStarted(true)}>Начать игру</button>
+                <div className="button-wrapper">
+                    <div className="button-wrapper-inner">
+                        {!IsGameStarted &&
+                            <button className='start-btn' onClick={startGame}>Начать игру</button>
+                        }
+                        <div className='game-over'>
+                            <h2>
+                                GameOver
+                            </h2>
+                            <p className='level'></p>
+                            <p className='score'></p>
+                            <button>Играть снова</button>
                         </div>
                     </div>
-                }
-                {IsGameStarted && <>
-                    <div className="button-wrapper">
-                        <div className="button-wrapper-inner">
-                            <button className='start-btn' onClick={() => setIsGameStarted(false)}>Вернуться назад</button>
-                        </div>
-                    </div>
-                </>
-                }
+                </div>
             </div>
             <div className='game-info'>
                 <canvas ref={canvasRefFigure} id="canvas-figure" ></canvas>
-                <div className='game-score'>
-                    сама фигура
-                </div>
+                <img className='avatar' src={foto} alt="" />
+                <p>Счет</p>
+                <p>Уровень</p>
+                <p>Линии</p>
             </div>
+            {/* следующая фигура */}
         </div>
     )
 };
