c7adefbe8538dd6d6ae052b5d3b9205d0bf8e47b
Nikita <nekitrus92@yandex.ru>
2022-11-18 14:00:26 +0300

    ssr-try

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 import { VitePWA } from 'vite-plugin-pwa';
+import rollupReplace from '@rollup/plugin-replace';
+import * as path from 'path';
 dotenv.config();
 
 // https://vitejs.dev/config/
@@ -13,6 +15,13 @@ export default defineConfig({
     __SERVER_PORT__: process.env.SERVER_PORT,
   },
   plugins: [
+    rollupReplace({
+      preventAssignment: true,
+      values: {
+        __DEV__: JSON.stringify(true),
+        'process.env.NODE_ENV': JSON.stringify('development'),
+      },
+    }),
     react(),
     // eslint-disable-next-line new-cap
     VitePWA({
@@ -26,4 +35,13 @@ export default defineConfig({
       injectRegister: 'auto',
     }),
   ],
+  resolve: process.env.USE_SOURCE
+    ? {
+        alias: {
+          '@remix-run/router': path.resolve(__dirname, '../../packages/router/index.ts'),
+          'react-router': path.resolve(__dirname, '../../packages/react-router/index.ts'),
+          'react-router-dom': path.resolve(__dirname, '../../packages/react-router-dom/index.tsx'),
+        },
+      }
+    : {},
 });
--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,106 @@
 import dotenv from 'dotenv'
-import cors from 'cors'
-dotenv.config()
-
+// import cors from 'cors'
+// import { createClientAndConnect } from './db'
+import path from 'path'
+import fsp from 'fs/promises'
 import express from 'express'
-import { createClientAndConnect } from './db'
+import type { ViteDevServer } from 'vite'
+// const app = express()
+dotenv.config()
+// @ts-ignore
+import { render } from '../client/dist/ssr/entry-server.cjs'
+console.log(render)
 
-const app = express()
-app.use(cors())
+function resolve(p: any) {
+  return path.resolve(__dirname, p)
+}
 const port = Number(process.env.SERVER_PORT) || 3001
+async function createServer(
+  root = process.cwd(),
+  isProduction = process.env.NODE_ENV === 'production'
+) {
+  const app = express()
+  /**
+   * @type {import('vite').ViteDevServer}
+   */
+  let vite: ViteDevServer
+  if (!isProduction) {
+    vite = await (
+      await import('vite')
+    ).createServer({
+      root,
+      server: {
+        middlewareMode: true,
+      },
+      appType: 'custom',
+    })
 
-createClientAndConnect()
+    app.use(vite.middlewares)
+  } else {
+    app.use((await import('compression')).default())
+    app.use(express.static(resolve('dist/client')))
+  }
+  app.get('/', (_, res) => {
+    res.json('👋 Howdy from the server :)')
+  })
 
-app.get('/', (_, res) => {
-  res.json('👋 Howdy from the server :)')
+  app.get('/ssr-example', (_, res) => {
+    const result = render()
+    res.send(result)
+  })
+  app.use('*', async (req: any, res: any) => {
+    const url = req.originalUrl
+
+    try {
+      let template
+      let render
+
+      if (!isProduction) {
+        template = await fsp.readFile(resolve('index.html'), 'utf8')
+        template = await vite.transformIndexHtml(url, template)
+        render = await vite
+          .ssrLoadModule('../packages/client/src/entry-server.tsx')
+          .then((m: Record<string, any>) => m.render)
+      } else {
+        template = await fsp.readFile(
+          resolve('../packages/client/dist/client/index.html'),
+          'utf8'
+        )
+        render = (
+          // @ts-ignore
+          await import('../client/dist/ssr/entry-server.js')).render
+      }
+
+      const html = template.replace('<!--root-html-->', render(url))
+      res.setHeader('Content-Type', 'text/html')
+      return res.status(200).end(html)
+    } catch (error: any) {
+      if (!isProduction) {
+        vite.ssrFixStacktrace(error)
+      }
+      console.log(error.stack)
+      res.status(500).end(error.stack)
+    }
+  })
+
+  return app
+}
+
+createServer().then(app => {
+  app.listen(port, () => {
+    console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
+  })
 })
 
-app.listen(port, () => {
-  console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
-})
+// app.use(cors())
+// const port = Number(process.env.SERVER_PORT) || 3001
+
+// createClientAndConnect()
+
+// app.get('/', (_, res) => {
+//   res.json('👋 Howdy from the server :)')
+// })
+
+// app.listen(port, () => {
+//   console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
+// })
