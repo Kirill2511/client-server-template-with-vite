1d520fb220ecd3b105ba69b047b1d196883a382a
Kirill <kirill.kirill2@yandex.ru>
2022-11-29 14:27:55 +0500

    hotfix

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 import { VitePWA } from 'vite-plugin-pwa';
-import rollupReplace from '@rollup/plugin-replace';
-import * as path from 'path';
 
 dotenv.config();
 
@@ -20,13 +18,6 @@ export default defineConfig({
     format: 'cjs',
   },
   plugins: [
-    rollupReplace({
-      preventAssignment: true,
-      values: {
-        __DEV__: JSON.stringify(true),
-        'process.env.NODE_ENV': JSON.stringify('development'),
-      },
-    }),
     react(),
     // eslint-disable-next-line new-cap
     VitePWA({
@@ -40,13 +31,4 @@ export default defineConfig({
       injectRegister: 'auto',
     }),
   ],
-  resolve: process.env.USE_SOURCE
-    ? {
-        alias: {
-          '@remix-run/router': path.resolve(__dirname, '../../packages/router/index.ts'),
-          'react-router': path.resolve(__dirname, '../../packages/react-router/index.ts'),
-          'react-router-dom': path.resolve(__dirname, '../../packages/react-router-dom/index.tsx'),
-        },
-      }
-    : {},
 });
